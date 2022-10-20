0ba1f3dfc22a82ddd76428dd199f2b5dfc63b5fd
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-10-20 14:58:56 +0200

    remove fontawesome

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" />
             </Head>
             <body>
                 <Main />
