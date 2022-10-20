0482c0aa072c088a609444fdf44f2b0bca3fa657
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-10-20 15:03:58 +0200

    restore fontawesome

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" />
             </Head>
             <body>
                 <Main />
