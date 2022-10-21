a505d3de3acaddea6479b3e2f1b3b82cacf6b3ab
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-10-21 15:11:59 +0200

    comment getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -11,23 +11,23 @@ import Trips from '../components/home/Trips';
 import axios from "axios";
 
 
-export const getServerSideProps = async () => {
-  try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/trips`);
-    const res2 = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/camps`);
-    return {
-      props: {
-        trips: res.data,
-        camps: res2.data
-      }
-    };
-  }
-  catch {
-    return {
-      notFound: true,
-    }
-  }
-};
+// export const getServerSideProps = async () => {
+//   try {
+//     const res = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/trips`);
+//     const res2 = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/camps`);
+//     return {
+//       props: {
+//         trips: res.data,
+//         camps: res2.data
+//       }
+//     };
+//   }
+//   catch {
+//     return {
+//       notFound: true,
+//     }
+//   }
+// };
 
 
 export default function Home({ trips, camps, language, onLinkClick }) {
