50b0bca1b470632b4dac71616236fee25c7cb2e2
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-10-21 18:31:08 +0200

    log productuon token to the console

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -8,7 +8,7 @@ const authenticate = (handler) => {
         const token = req.headers.cookie && (
             process.env.NEXT_PUBLIC_CURRENT_ENV !== "production" 
             ? req.headers.cookie.split("=")[1]
-            : req.headers.cookie.split("token=")[1]
+            : req.headers.cookie.split("=")[1]
         );
         // console.log("MyToken", token)
         if (!token) throw createError(401, "You are not authenticated");
@@ -16,7 +16,7 @@ const authenticate = (handler) => {
         verify(token, process.env.NEXT_PUBLIC_JWT_SECRET, (error, user) => {
             if (error) throw createError(403, "Token is not valid");
             req.user = user;
-            console.log(error)
+            console.log(token)
         })
 
         return handler(req, res);
