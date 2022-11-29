8a898543fe4fcd7b0906e1eb31b4f17c2d511500
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-11-29 06:47:08 +0200

    edit-responsiveness

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require('http')
 const app = next(__dirname)
 const handle = app.getRequestHandler()
 
-// init normal http server
+// init normal http server -
 // createServer(handle).listen(process.env.PORT || 3000)
 
 
