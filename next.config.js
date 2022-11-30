33f525ce7b617ba181b16dcb9b3ef71eeca041bc
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-11-30 12:01:18 +0200

    reduce-build-size

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      'res.cloudinary.com'
-    ],
-  }
-}
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-module.exports =  nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  env: {
+    NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: [
+        'res.cloudinary.com'
+      ],
+    }
+  },
+})
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = next(__dirname)
 const handle = app.getRequestHandler()
 
 // init normal http server -
-createServer(handle).listen(process.env.PORT || 3000)
+// createServer(handle).listen(process.env.PORT || 3000)
 
 
 
