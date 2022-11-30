9dfc05d274cbad9885073fc09b470d904278ecbd
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-11-30 10:47:33 +0200

    preact-3

--- next.config.js
+++ next.config.js
@@ -13,21 +13,6 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      'i.postimg.cc',
-      'www.picng.com',
-      'images.akhbarelyom.com',
-      'egyptianstreets.com',
-      'i.pinimg.com',
-      'd3rr2gvhjw0wwy.cloudfront.net',
-      'dsvsbigncb06y.cloudfront.net',
-      'www.urtrips.com',
-      'www.abou-alhool.com',
-      'cf.bstatic.com',
-      'q-xx.bstatic.com',
-      'hgtvhome.sndimg.com',
-      'www.devasom.com',
-      'media.istockphoto.com',
-      'i0.wp.com',
       'res.cloudinary.com'
     ],
   }
